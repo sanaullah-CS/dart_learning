76d704324889ff2dda037c0b9a72ef08d85ea0c2
sanaullah-CS <sanaulah101@gmail.com>
2026-08-18 18:05:34 +0500

    Initial Flutter project

---
+++ lib/practice.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main()
+
+{
+  int a = 7 ;
+  print(a);
+  BigInt longvalue;
+  longvalue = BigInt.parse('5879654166666578451236985236547812365478954123025874102366987410236547896987042');
+  num percenatge = 33.65;
+bool islogin = false;
+
+  print(longvalue);
+  }
+
+class Human //classs human
+
+    {
+
+  Human();
+
+    }
