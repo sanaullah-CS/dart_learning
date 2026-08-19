062007410b0b00b39fcf9bd981fb5e6cb48fd467
sanaullah-CS <sanaulah101@gmail.com>
2026-08-19 18:23:53 +0500

    Day 1: Dart fundamentals and variables

---
+++ lib/day1/practice_calculator.dart
@@ -0,0 +1,21 @@
+void main ()
+{
+  print("Hello This is Students record");
+  String name= "SANAULLAH AWAN";
+  int age = 22;
+  double cgpa=3.41;
+  bool isGraduate = true;
+  var city = "Abbottabad";
+  dynamic status= "Graduate";
+  final university ="AUST";
+  const pi= 3.1429378;
+  print("Student name:$name");
+  print("Age:$age");
+  print("CGPA:$cgpa");
+  print("Graduate:$isGraduate");
+  print("city:$city");
+  print("University:$university");
+  print("PI:$pi");
+  print("HI THIS IS ME $name AND I'M $age YEARS OLD. I RECENTLY  $isGraduate FROM $university. MY CGPA IS $cgpa AND BASIICALLY I'M FROM $city");
+
+}
