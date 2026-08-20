db3b5c461c19b7b18b50a086eecdc5bc85439797
sanaullah-CS <sanaulah101@gmail.com>
2026-08-20 10:34:15 +0500

    Day 1 fundamentals and variables

---
+++ lib/day1/task 1.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  print("----------------------------");
+  print("STUDENT INFORMATION".padLeft(22));
+  print("----------------------------");
+  var name = "SANAULLAH";
+  int age= 21;
+  var Address="POF ESTATE HAVELIAN CANTT";
+  var city="HAVELIAN";
+  dynamic status="GRADUATE";
+  int phone= 923359587236;
+  var skills = "FLUTTER EXPERT, PYTHON, C++, JAVA SCRIPT, AI , MACHINE LEARNING, etc.";
+  print("STUDENT NAME IS : $name");
+  print("HOW OLD IS HE? $age");
+  print("ADDRESS: $Address");
+  print("CITY:$city");
+  print("STATUS:$status");
+  print("CELL no.:$phone");
+  print("OVERALL SKILLS:$skills");
+}
