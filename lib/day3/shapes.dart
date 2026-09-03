33436516e01a5066c26d426adb0e16a0e5fbc9c8
sanaullah-CS <sanaulah101@gmail.com>
2026-09-03 15:55:19 +0500

    NEW DAY

--- lib/day3/shapes.dart
+++ lib/day3/shapes.dart
@@ -33,7 +33,7 @@ void main() {
   print("*        *");
   print("**********");
   //this is my 5th task
-  print ("create a circle shape");
+  print("create a circle shape");
   print("   ***   ");
   print(" *      *");
   print("*        *");
@@ -52,27 +52,59 @@ void main() {
   print("   *   ");
   //this is my 7th task
   print("create a pyramid shape");
-  print("        *\n       ***\n      *****\n     *******\n    *********\n   ***********");
+  print(
+      "        *\n       ***\n      *****\n     *******\n    *********\n   ***********");
   print("Enter Your age:");
   int age = int.parse(stdin.readLineSync()!);
   print("Your Age is: $age");
-  if (age >= 22)
-  {
-  print("u are Minor");
+  if (age >= 22) {
+    print("u are Minor");
   }
-  else{
+  else {
     print("U are an adult");
   }
   print("enter ur name:");
   String name = stdin.readLineSync()!;
-  if (name=="Sanaullah")
-  {
+  if (name == "Sanaullah") {
     print("ur name is correct");
   }
-  else{
+  else {
     print("Ur name is not correct");
   }
-  String realName="SANAULLAH AWAN";
+  String realName = "SANAULLAH AWAN";
   print("your real name is:$realName");
   print("please try again thanks");
+
+  var admin = true;
+  var manager = false;
+  print("login page");
+  print("Apply for Access");
+  print("ENTER USERNAME");
+  print("ENTER PASSWORD");
+  if (admin || manager) {
+    print("Access is granted");
+    print("welcome to WEBSITE");
+    print("NOW DO YOUR WORK");
+  }
+  else {
+    print("ACCESS NOT GRANTED");
+    print("TRY AGAIN PLEASE");
+  }
+  var isadmin = false;
+  if (!isadmin) {
+    print("please login first");
+    print("now enter user name");
+    print("----------");
+    var userName = stdin.readLineSync()!;
+    print("----------");
+    print("now enter password");
+    print("----------");
+    var password = stdin.readLineSync()!;
+    print("----------");
+    print("ACCESS GRANTED");
+    print("welcome to website");
+  } else {
+    print("ACCESS NOT GRANTED");
+    print("PLEASE TRY AGAIN");
+  }
 }
